069c7185a48f8ac6a13382e9363daff400b72ebc
Alistair <alistairm2022@gmail.com>
2022-11-21 14:21:15 +0530

    Working search news

--- app/src/main/java/com/alistair/newsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/alistair/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -1,21 +1,32 @@
 package com.alistair.newsapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.alistair.newsapp.R
-import com.alistair.newsapp.databinding.FragmentBreakingNewsBinding
+import com.alistair.newsapp.adapters.NewsAdapter
 import com.alistair.newsapp.databinding.FragmentSearchNewsBinding
 import com.alistair.newsapp.ui.NewsActivity
 import com.alistair.newsapp.ui.NewsViewModel
+import com.alistair.newsapp.util.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.alistair.newsapp.util.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class SearchNewsFragment: Fragment() {
+class SearchNewsFragment : Fragment() {
     private var _binding: FragmentSearchNewsBinding? = null
     private val binding get() = _binding!!
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    val TAG = "SearchNewsFragment"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +40,57 @@ class SearchNewsFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        var job: Job? = null
+        binding.etSearch.addTextChangedListener { editable->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+
+                editable?.let{
+                    if(editable.toString().isNotEmpty()){
+                        viewModel.searchNews(editable.toString())
+                    }
+                }
+
+            }
+        }
+        viewModel.searchNews.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, " AN error occurred")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        }
+    }
+
+    private fun hideProgressBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/alistair/newsapp/util/Constants.kt
+++ app/src/main/java/com/alistair/newsapp/util/Constants.kt
@@ -4,6 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "d85f93657fc84679b8fba779c998dbd4"
         const val BASE_URL = "https://newsapi.org"
-
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
